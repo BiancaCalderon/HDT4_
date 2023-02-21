d1a696cfc88093add427f9a5aa72d5efa85592ec
Bianca Calderón <98110978+BiancaCalderon@users.noreply.github.com>
2023-02-20 21:33:18 -0600

    VectorStack

---
+++ src/VectorStack.java
@@ -0,0 +1,46 @@
+import java.util.Vector;
+
+public class VectorStack<T> extends AbstractStack<T> {
+    private Vector<T> element;
+
+    public VectorStack() {
+        element = new Vector<T>();
+        size = 0;
+    }
+
+    public void apilar(T elemento) {
+        element.add(elemento);
+        size++;
+    }
+
+    public T desapilar() {
+        if (size == 0) {
+            throw new RuntimeException("Pila vacía");
+        }
+        T elemento = element.remove(size-1);
+        size--;
+        return elemento;
+    }
+
+    public T cima() {
+        if (size == 0) {
+            throw new RuntimeException("Pila vacía");
+        }
+        return element.get(size-1);
+    }
+
+    @Override
+    public void push(T element) {
+
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+}
